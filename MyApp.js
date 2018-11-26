d452a538b3c426e669b42947594b478e1569ac41
VimalRaj Selvam <email2vimalraj@gmail.com>
2018-11-26 23:54:27 +0800

    Added hamburger menu

--- MyApp.js
+++ MyApp.js
@@ -1,12 +1,52 @@
 import React from "react";
 import { View, Text } from "react-native";
 import { createDrawerNavigator, createAppContainer } from "react-navigation";
+import { Header, Icon } from "react-native-elements";
+
+class HamburgerMenu extends React.Component {
+  render() {
+    return (
+      <Icon
+        name="menu"
+        color="#fff"
+        onPress={() => {
+          this.props.navigation.toggleDrawer();
+        }}
+      />
+    );
+  }
+}
 
 class HomeScreen extends React.Component {
   render() {
+    console.log(this.props.navigation);
     return (
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <View>
+        <Header
+          statusBarProps={{ barStyle: "light-content" }}
+          leftComponent={<HamburgerMenu navigation={this.props.navigation} />}
+          centerComponent={{ text: "MY TITLE", style: { color: "#fff" } }}
+          rightComponent={{ icon: "home", color: "#fff" }}
+          outerContainerStyles={{ height: 80 }}
+        />
         <Text>Home Screen</Text>
+      </View>
+    );
+  }
+}
+
+class SettingsScreen extends React.Component {
+  render() {
+    return (
+      <View>
+        <Header
+          statusBarProps={{ barStyle: "light-content" }}
+          leftComponent={<HamburgerMenu navigation={this.props.navigation} />}
+          centerComponent={{ text: "MY TITLE", style: { color: "#fff" } }}
+          rightComponent={{ icon: "home", color: "#fff" }}
+          outerContainerStyles={{ height: 80 }}
+        />
+        <Text>Settings Screen</Text>
       </View>
     );
   }
@@ -15,6 +55,9 @@ class HomeScreen extends React.Component {
 const AppNavigator = createDrawerNavigator({
   Home: {
     screen: HomeScreen
+  },
+  Settings: {
+    screen: SettingsScreen
   }
 });
 
